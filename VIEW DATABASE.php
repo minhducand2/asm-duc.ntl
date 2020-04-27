155bb6da0e70fbdaeabed6b856442d2ce8497913
Phạm Văn Minh Đức <55387191+minhducand2@users.noreply.github.com>
2020-04-27 14:10:08 +0700

    ANT

---
+++ VIEW DATABASE.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+td, th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
+
+tr:nth-child(even) {
+  background-color: #dddddd;
+}
+</style>
+</head>
+<body>
+
+<h2>VIEW DATABASE</h2>
+
+<table>
+  <tr>
+    <th>Employee ID</th>
+    <th>Full name</th>
+    <th>Email</th>
+    <th>Phone number</th>
+  </tr>
+  <tr>
+    <td>1000</td>
+    <td>Maria Anders</td>
+    <td>germany@gmail.com</td>
+    <td>0236113</td>
+  </tr>
+  <tr>
+    <td>1001</td>
+    <td>Francisco Chang</td>
+    <td>mexico@gmail.com</td>
+    <td>0236115</td>
+  </tr>
+  <tr>
+    <td>1002</td>
+    <td>Roland Mendel</td>
+    <td>austria@gmail.com</td>
+    <td>0236114</td>
+  </tr>
+</table>
+
+</body>
+</html>
